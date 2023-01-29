e01823bfc5497e1a776f70b44ae6f5c2a4fd08ec
John Garcia <jhongarcian@jhongarcian.local>
2023-01-29 14:29:28 -0600

    reverse commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ export const ThemeContext = React.createContext();
 
 function App() {
   const [theme, setTheme] = useState("green");
-
-  function setTheme(prevTheme) {
-    return prevTheme === "red" ? "blue" : "red";
-  }
   return (
     <ThemeContext.Provider value={{ backgroundColor: theme }}>
       <Counter inicialCount={0}></Counter>
-      <button onClick={setTheme}>Toogle Theme</button>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "red" ? "blue" : "red";
+          })
+        }
+      >
+        Toogle Theme
+      </button>
     </ThemeContext.Provider>
   );
 }
